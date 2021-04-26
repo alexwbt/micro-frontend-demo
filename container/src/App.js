32c43e25ce51d9c6ab50e72834162960bcb08a38
alex.wong <723@ssfill.net>
2021-04-26 11:06:05 +0800

    added app3 combine micro frontends

--- container/src/App.js
+++ container/src/App.js
@@ -12,16 +12,20 @@ const Header = styled.div`
 
 const App1 = () => <MicroContainer name="app-1" origin="http://localhost:3001/" />
 const App2 = () => <MicroContainer name="app-2" origin="http://localhost:3002/" />
+const App3 = () => <div><App1 /><App2 /></div>;
 
 const App = () => <BrowserRouter>
     <Fragment>
         <Header>
+            <Link to="/">Home</Link>
             <Link to="/app1">App1</Link>
             <Link to="/app2">App2</Link>
+            <Link to="/app3">App3</Link>
         </Header>
         <Switch>
             <Route exact path='/app1' component={App1} />
             <Route exact path='/app2' component={App2} />
+            <Route exact path='/app3' component={App3} />
         </Switch>
     </Fragment>
 </BrowserRouter>;
